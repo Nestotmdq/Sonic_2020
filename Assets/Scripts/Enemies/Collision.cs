e5b9da8136e01373398111ad68afb9a95e8e40bc
Nestor <nestoroteromdq@gmail.com>
2020-05-06 03:20:15 -0300

    agregando script de enemigos

---
+++ Assets/Scripts/Enemies/Collision.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Collision : MonoBehaviour {
+
+	public GameObject soundImpact;
+
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "player"){
+			
+            if(PlayerControler.playerControler.GetcanHit() == true){
+             Debug.Log("Sonic ataca");
+				gameObject.GetComponent<Animator>().SetBool("fire",true);
+				Destroy(gameObject,1);
+				Instantiate(soundImpact);
+			}
+
+		}
+
+	}
+
+
+	/* 
+  void OnCollisionEnter2D(Collision2D other) {
+   if(other.gameObject.tag == "player"){
+	   Debug.Log("Phantom fue impactado por sonic");
+	   if(PlayerControler.playerControler.GetcanHit() == true){
+		   Debug.Log("muere phantom");
+		    gameObject.GetComponent<Animator>().SetBool("fire",true);
+		  Destroy(gameObject,1);
+	   }
+   }
+ }
+*/
+
+
+
+
+
+}
--- Assets/Scripts/Sonic/PlayerControler.cs
+++ Assets/Scripts/Sonic/PlayerControler.cs
@@ -10,36 +10,48 @@ public class PlayerControler : MonoBehaviour {
 	public float fuerza;
 	public float fuerzaSalto;
 	bool canJump;
-    Animator an;
+    bool canHit;
+	Animator an;
 	Rigidbody2D rb;
 	SpriteRenderer sr;
+	public GameObject soundHit;
+	bool keyActive;
+	BoxCollider2D bc;
+	
 	void Start(){
 		
 		playerControler = this;
 		an = gameObject.GetComponent<Animator>();
 		rb = gameObject.GetComponent<Rigidbody2D>();
 		sr = gameObject.GetComponent<SpriteRenderer>();
+		bc = gameObject.GetComponent<BoxCollider2D>();
+		keyActive = true;
 	}
 	private void FixedUpdate () {
-		if(Input.GetKey("left")){
-				                 left();
-                         		}
-		if(Input.GetKey("right")){
-							   	 right();
-		                         }
-		if(Input.GetKeyDown("up") && canJump){
-                                              up(fuerzaSalto);
-		                                     }
-		if(!Input.GetKey("left") && !Input.GetKey("right")){
-		                                                	an.SetBool("moving",false);
-														   }
-		if(Input.GetKey("left") && Input.GetKeyDown("up")){
-															runnerjump();
-                            					    		}
-		if(Input.GetKey("right") && Input.GetKeyDown("up")){
-															runnerjump();
-		                                                   }													
-	}
+
+                                 if(keyActive == true){
+		                                               if(Input.GetKey("left")){
+				                                                                left();
+                         	                                                	}
+		                                               if(Input.GetKey("right")){
+							                                                	 right();
+		                                                                        }
+		                                               if(Input.GetKeyDown("up") && canJump){
+                                                                                            up(fuerzaSalto);
+		                                                                                    }
+	                                            	if(!Input.GetKey("left") && !Input.GetKey("right")){
+		                                                                                             	an.SetBool("moving",false);
+													                                             	   }
+	                                            	if(Input.GetKey("left") && Input.GetKeyDown("up")){
+														                                            	runnerjump();
+                            					    	                                            	}
+	                                            	if(Input.GetKey("right") && Input.GetKeyDown("up")){
+														                                            	runnerjump();
+		                                                                                                }
+
+			                                           }
+
+	                            }
 	private void left(){
                 		rb.AddForce(new Vector2(fuerza * -1 * Time.deltaTime,0));
 		                sr.flipX = true;
@@ -61,6 +73,7 @@ public class PlayerControler : MonoBehaviour {
 					             canJump = false;
 					             rb.AddForce(new Vector2(0,fuerza));
 					             an.SetBool("jump",true);
+								 canHit = true;
 								 }
 	void OnCollisionEnter2D(Collision2D coll){
 	                                          if(coll.transform.tag == "ground"){
@@ -68,9 +81,49 @@ public class PlayerControler : MonoBehaviour {
 										                                         an.SetBool("jump",false);
 										                                         an.SetBool("movjump",false);
 									                                             an.SetBool("caida",false);
+																				 canHit = false;
 	                                                                            }
-	                                         }
+																				/* 
+											 if(coll.transform.tag == "phanton"){
+
+												 	Debug.Log("Choque con fantom");
+													 if(canJump == true){
+														 Destroy(gameObject);
+													 }
+
+                                                        */
+											 }
+											  	
+
+	 void OnTriggerEnter2D(Collider2D other) {
+			if(other.transform.tag == "phanton"){
+                if(canJump == true){
+                    Instantiate(soundHit);
+                      keyActive = false;
+					  StartCoroutine("Wait");
+					  an.SetBool("inertia",true);
+		              an.SetBool("wakeup",false);
+		              an.SetBool("caida",true);
+					//  rb.AddForce(new Vector2(0,1000f));
+					  bc.enabled = false;
+					  //bc.enabled = true;
+
+				}
+				
+			}
+	}
+    IEnumerator Wait(){
+                       yield return new WaitForSeconds(1);
+	                  bc.enabled = true;
+					  keyActive = true;	                  }
+
+
     public bool GetcanJump(){
 	                        return canJump;
                             }
+	public bool GetcanHit(){
+							return canHit;
+							}
+
+
                                              }
