19b94c1f061b0001516e2802f8a31c5d1f834c69
Nestor <nestoroteromdq@gmail.com>
2020-05-17 00:43:48 -0300

    agregue un enemigo

---
+++ Assets/Scripts/Enemies/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float velocity = 2f;
+	private Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.left * velocity;
+
+	}
+}
