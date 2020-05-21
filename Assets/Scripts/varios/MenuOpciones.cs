6cd83a9e950c29d939ba2dd5b2e9177ee0d8ba0c
Nestor <nestoroteromdq@gmail.com>
2020-05-20 23:38:49 -0300

    agregue presentacion y cambie collider de sonic por circle collider

---
+++ Assets/Scripts/varios/MenuOpciones.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOpciones : MonoBehaviour {
+
+	public void Jugar(){
+
+		SceneManager.LoadScene("first");
+	}
+
+	public void Salir(){
+
+		Application.Quit();
+	}
+}
