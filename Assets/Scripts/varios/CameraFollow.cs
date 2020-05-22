1363ff2caa13ef531a79b752f4954ef0553c6425
Nestor <nestoroteromdq@gmail.com>
2020-05-21 23:03:43 -0300

    agregando informacion al canvas

--- Assets/Scripts/varios/CameraFollow.cs
+++ Assets/Scripts/varios/CameraFollow.cs
@@ -9,22 +9,21 @@ public GameObject follow;
 Vector2 velocity;
 public float smoothTime;
 
-void FixedUpdate () {
+
+void FixedUpdate(){
+              Follow();
+
+}
+
+void Follow(){
                                      float posX = Mathf.SmoothDamp(transform.position.x, 
                                      follow.transform.position.x, ref velocity.x, smoothTime);
                                      float posY = Mathf.SmoothDamp(transform.position.y, 
                                      follow.transform.position.y, ref velocity.y, smoothTime);
-
                                      transform.position = new Vector3(
                                      Mathf.Clamp(posX, minCampos.x , maxCampos.x),
                                      Mathf.Clamp(posY, minCampos.y , maxCampos.y),
-                                     transform.position.z
-                                     );
+                                     transform.position.z);
+                                     
 }
-
-
-
-
-
-
 }
